9696517e4591ed5ff2cd8278a77d0210037052e8
Clara Kim <tiffanykim15@Clara-Kim-3.local>
2014-10-04 18:54:36 -0700

    Initial Commit

---
+++ raceCar/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  raceCar
+//
+//  Created by Clara Kim on 10/4/14.
+//  Copyright (c) 2014 Clara Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
